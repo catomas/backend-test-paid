de9cf67390556095683b65bbce255fce40e73a8b
Juan Camilo Echeverri Salazar <juancami98.11@hotmail.com>
2024-09-18 16:59:25 -0500

    Refactor CORS configuration to allow requests from multiple origins

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   app.setGlobalPrefix("api");
 
   app.enableCors({
-    origin: "http://localhost:3001", // Allow requests from this origin
+    origin: [
+      "http://localhost:3001",
+      "https://test-paid-e0ea7a7b42f0.herokuapp.com",
+    ], // Allow requests from this origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
