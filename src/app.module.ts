6aca269c59f7423bfca8062883a9f221f42ca166
Juan Camilo Echeverri Salazar <juancami98.11@hotmail.com>
2024-09-18 15:41:45 -0500

    Refactor database configuration to support SSL in production environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,11 @@ import { UserModule } from "./users/users.module";
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === "prod",
+      extra: {
+        ssl:
+          process.env.STAGE === "prod" ? { rejectUnauthorized: false } : null,
+      },
       type: "postgres",
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
